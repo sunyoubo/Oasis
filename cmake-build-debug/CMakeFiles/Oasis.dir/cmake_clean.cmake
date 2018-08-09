41e3d7a75879bb80c0d68b135fb2f2dc56c93431
sunyoubo <sunyoubo@digcredit.com>
2018-08-09 11:03:03 +0800

    添加Players、Marketplace合约

---
+++ cmake-build-debug/CMakeFiles/Oasis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oasis.dir/main.cpp.o"
+  "Oasis.pdb"
+  "Oasis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Oasis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
